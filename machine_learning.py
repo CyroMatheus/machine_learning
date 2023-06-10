5770990e151e86d12ab827dac2894de45322c6e8
CyroMatheus <mathorycbg@gmail.com>
2023-06-10 20:21:44 -0300

    logistic_regression

--- machine_learning.py
+++ machine_learning.py
@@ -2,6 +2,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from yellowbrick.classifier import ConfusionMatrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -15,6 +16,28 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import pickle
+
+def statistics(method, data_base, trainer, x_treinamento, x_teste, y_treinamento, y_teste, previsoes, previsores):
+    cm = ConfusionMatrix(trainer)
+    cm.fit(x_treinamento, y_treinamento)
+
+    print(f"\n{method}_{data_base}")
+    ac_store = cm.score(x_teste, y_teste)*100
+    print("accuracy_score: %.2f" % ac_store, "%")
+    pprint.pprint(f"confusion_matrix: {confusion_matrix(y_teste, previsoes)}")
+    print("classification_report: ")
+    print(classification_report(y_teste, previsoes))
+
+    try:
+        names_class = list()
+        for name_class in trainer.classes_:
+            names_class.append(str(name_class))
+
+        fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(20, 20))
+        fig.savefig(f'{data_base}_{method}.png')
+        shutil.move(f'{os.getcwd()}//{data_base}_{method}.png', f'{os.getcwd()}/data/imagens/{data_base}_{method}.png')
+    except Exception as e:
+        print(str(e), e.args)
 
 class MachineLearning():
     def __init__(self):
@@ -75,106 +98,60 @@ class MachineLearning():
         #
         #     with open(f'{os.getcwd()}/data/my_progress/credit.pkl', 'rb') as f:
         #         self.X_census_treinamento, self.y_census_treinamento, self.X_census_teste, self.y_census_teste = pickle.load(f)
+        #
+        #     with open(f'{os.getcwd()}/data/my_progress/risco_credito.pkl', 'rb') as f:
+        #         self.X_risco_credito, self.y_risco_credito = pickle.load(f)
 
-    def statistics(self, method, data_base, trainer, x_treinamento, x_teste, y_treinamento, y_teste, previsoes, previsores):
-        cm = ConfusionMatrix(trainer)
-        cm.fit(x_treinamento, y_treinamento)
+    def treinner(self):
+        list_methods = {
+            "naive_bayes": GaussianNB(),
+            "decision_tree": DecisionTreeClassifier(criterion="entropy", random_state=0),
+            "random_forest": RandomForestClassifier(n_estimators=40, criterion="entropy", random_state=0),
+            "knn":  KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=2),
+            "logistic_regression": LogisticRegression(random_state=1),
+            # "svm": "",
+        }
 
-        print(f"\n{method}_{data_base}")
-        print(f"accuracy_score: {cm.score(x_teste, y_teste)}")
-        pprint.pprint(f"confusion_matrix: {confusion_matrix(y_teste, previsoes)}")
-        print("classification_report: ")
-        print(classification_report(y_teste, previsoes))
-        if (method != "naive_bayes" and data_base != "credit_risk") and (method != "random_forest"):
-            names_class = list()
-            for name_class in trainer.classes_:
-                names_class.append(str(name_class))
+        data_base = {
+            "credit": {
+                "treinamento": {
+                    "X": self.X_credit_treinamento,
+                    "y": self.y_credit_treinamento
+                },
+                "teste": {
+                    "X": self.X_credit_teste,
+                    "y": self.y_credit_teste
+                },
+            },
+            "census": {
+                "treinamento": {
+                    "X": self.X_census_treinamento,
+                    "y": self.y_census_treinamento
+                },
+                "teste": {
+                    "X": self.X_census_teste,
+                    "y": self.y_census_teste
+                },
+            }
+        }
 
-            fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(20, 20))
-            if method == "decision_tree" and data_base == "census":
-                tree.plot_tree(trainer, class_names=names_class, filled=True)
-            else:
-                tree.plot_tree(trainer, feature_names=previsores, class_names=names_class, filled=True)
-            fig.savefig(f'{data_base}_{method}.png')
-            shutil.move(f'{os.getcwd()}//{data_base}_{method}.png', f'{os.getcwd()}/data/imagens/{data_base}_{method}.png')
+        previsores = {
+            "credit": ["age", "workclass", "final-weight", "education", "education-num", "marital-status", "occupation",
+                   "relationship", "race", "sex", "capital-gain", "capital-loos", "hour-per-week", "native-country"],
+            "census": ['income', 'age', 'loan']
+        }
 
-    def naive_bayes(self, method,  data_base, previsores):
-        if data_base == "credit_risk":
-            naive_risco_credito = GaussianNB()
-            naive_risco_credito.fit(self.X_risco_credito, self.y_risco_credito)
-            previsao = naive_risco_credito.predict([[0, 0, 1, 2], [2, 0, 0, 0]])
-            print(f"\n{method}_{data_base}: {previsao}")
-        elif data_base == "credit":
-            naive_credit_data = GaussianNB()
-            naive_credit_data.fit(self.X_credit_treinamento, self.y_credit_treinamento)
-            previsoes = naive_credit_data.predict(self.X_credit_teste)
-            self.statistics(method, data_base, naive_credit_data, self.X_credit_treinamento, self.X_credit_teste, self.y_credit_treinamento, self.y_credit_teste, previsoes, previsores)
-        elif data_base == "census":
-            naive_census_data = GaussianNB()
-            naive_census_data.fit(self.X_census_treinamento, self.y_census_treinamento)
-            previsoes = naive_census_data.predict(self.X_census_teste)
-            self.statistics(method, data_base, naive_census_data, self.X_census_treinamento, self.X_census_teste, self.y_census_treinamento, self.y_census_teste, previsoes, previsores)
-
-    def decision_tree(self, method,  data_base, previsores):
-        if data_base == "credit_risk":
-            tree_credit_risk = DecisionTreeClassifier(criterion="entropy")
-            tree_credit_risk.fit(self.X_risco_credito, self.y_risco_credito)
-            previsao = tree_credit_risk.predict([[0, 0, 1, 2], [2, 0, 0, 0]])
-            print(f"\n{method}_{data_base}: {previsao}")
-        elif data_base == "credit":
-            tree_credit = DecisionTreeClassifier(criterion="entropy", random_state=0)
-            tree_credit.fit(self.X_credit_treinamento, self.y_credit_treinamento)
-            previsoes = tree_credit.predict(self.X_credit_teste)
-            self.statistics(method, data_base, tree_credit, self.X_credit_treinamento, self.X_credit_teste, self.y_credit_treinamento, self.y_credit_teste, previsoes, previsores)
-        elif data_base == "census":
-            tree_census = DecisionTreeClassifier(criterion="entropy", random_state=0)
-            tree_census.fit(self.X_census_treinamento, self.y_census_treinamento)
-            previsoes = tree_census.predict(self.X_census_teste)
-            self.statistics(method, data_base, tree_census, self.X_census_treinamento, self.X_census_teste, self.y_census_treinamento, self.y_census_teste, previsoes, previsores)
-
-    def random_forest(self, method,  data_base, previsores):
-        if data_base == "credit":
-            credit_random_forest = RandomForestClassifier(n_estimators=40, criterion="entropy", random_state=0)
-            credit_random_forest.fit(self.X_credit_treinamento, self.y_credit_treinamento)
-            previsoes = credit_random_forest.predict(self.X_credit_teste)
-            self.statistics(method, data_base, credit_random_forest, self.X_credit_treinamento, self.X_credit_teste, self.y_credit_treinamento, self.y_credit_teste, previsoes, previsores)
-        elif data_base == "census":
-            census_random_forest = RandomForestClassifier(n_estimators=40, criterion="entropy", random_state=0)
-            census_random_forest.fit(self.X_census_treinamento, self.y_census_treinamento)
-            previsoes = census_random_forest.predict(self.X_census_teste)
-            self.statistics(method, data_base, census_random_forest, self.X_census_treinamento, self.X_census_teste, self.y_census_treinamento, self.y_census_teste, previsoes, previsores)
-
-    def knn(self, method, data_base):
-        print(f"{method} {data_base}")
-        if data_base == "credit":
-            knn_credit = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=2)
-            knn_credit.fit(self.X_credit_treinamento, self.y_credit_treinamento)
-            previsoes = knn_credit.predict(self.X_credit_teste)
-
-            cm = ConfusionMatrix(knn_credit)
-            cm.fit(self.X_credit_treinamento, self.y_credit_treinamento)
-            print(f"accuracy_score: {cm.score(self.X_credit_teste, self.y_credit_teste)}")
-            pprint.pprint(f"confusion_matrix: {confusion_matrix(self.y_credit_teste, previsoes)}")
-        elif data_base == "census":
-            knn_census = KNeighborsClassifier(n_neighbors=5, metric="minkowski", p=2)
-            knn_census.fit(self.X_census_treinamento, self.y_census_treinamento)
-            previsoes = knn_census.predict(self.X_census_teste)
-
-            cm = ConfusionMatrix(knn_census)
-            cm.fit(self.X_census_treinamento, self.y_census_treinamento)
-            print(f"accuracy_score: {cm.score(self.X_census_teste, self.y_census_teste)}")
-            pprint.pprint(f"confusion_matrix: {confusion_matrix(self.y_census_teste, previsoes)}")
-
+        for method in list_methods:
+            methodExecutable = list_methods[method]
+            for db in data_base:
+                methodExecutable.fit(data_base[db]["treinamento"]["X"], data_base[db]["treinamento"]["y"])
+                previsoes = methodExecutable.predict(data_base[db]["teste"]["X"])
+                statistics(method, db, methodExecutable, data_base[db]["treinamento"]["X"], data_base[db]["teste"]["X"],
+                           data_base[db]["treinamento"]["y"], data_base[db]["teste"]["y"], previsoes, previsores[db])
 
 
 def launcher():
-    warehouse = ["credit", "census"]
-
     machine_learning = MachineLearning()
-    for data_base in warehouse:
-        machine_learning.knn("knn", data_base)
-        # machine_learning.naive_bayes("naive_bayes", data_base, warehouse[data_base])
-        # machine_learning.decision_tree("decision_tree", data_base, warehouse[data_base])
-        # machine_learning.random_forest("random_forest", data_base, warehouse[data_base])
+    machine_learning.treinner()
 
 launcher()
